eb6e45b4d183512b38db882986162e0b5353a771
jr200 <jayshan+git@gmail.com>
2025-08-06 10:18:41 +0100

    fix exported types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { natsMachine } from './machines/root'
+export { subjectManagerLogic, type Context as SubjectContext, type ExternalEvents as SubjectEvent } from './machines/subject'
+export { kvManagerLogic, type Context as KvContext, type ExternalEvents as KvEvent } from './machines/kv'
 export { KvSubscriptionKey, type KvSubscriptionConfig } from './actions/kv'
 export { parseNatsResult } from './actions/connection'
 export { type AuthConfig } from './actions/types'
-export { type NatsConnectionConfig, type Context, type ExternalEvents } from './machines/root'
+
+export { type NatsConnectionConfig, type Context as NatsContext, type ExternalEvents as NatsEvent } from './machines/root'
